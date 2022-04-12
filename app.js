24f5ae04af7d2c3daadfae80b20e87b1c299f56f
pierre alexandre pautrat <pap@pautrat.org>
2022-04-12 10:43:14 +0200

    variable port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/',(req,res) => {
     res.send("Welcome to the home page baby");
 })
-
+const port = process.env.port || 3000;
 app.listen(3000,() => {
     console.log("Wazappp");
 
